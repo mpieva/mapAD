4df11ec3280bc39c43e41366ffd87221559d2933
Christian Heide <christian_heide@eva.mpg.de>
2019-03-19 15:31:58 +0100

    Fix CLI gap costs

--- src/main.rs
+++ src/main.rs
@@ -81,8 +81,8 @@ fn main() {
             let alignment_parameters = utils::AlignmentParameters {
                 base_error_rate: 0.02,
                 poisson_threshold: value_t_or_exit!(map_matches.value_of("poisson_prob"), f64),
-                penalty_gap_open: 2.0,
-                penalty_gap_extend: 1.0,
+                penalty_gap_open: -2.0,
+                penalty_gap_extend: -1.0,
             };
             if let Err(e) = map::run(
                 map_matches.value_of("reads").unwrap(),
