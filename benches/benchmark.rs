0455c4a7e2ad6e14c0dd40b2b1a109491047cbf4
Christian Heide <christian_heide@eva.mpg.de>
2019-06-25 15:57:38 +0200

    Add more useful benchmarks. This improves the meaningfulness of the results. Both, a set of mapping, and a set of non-mapping reads are benchmarked now.

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -72,5 +72,166 @@ fn criterion_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn bench_multiple_reads(c: &mut Criterion) {
+    c.bench_function("bench_multiple_reads", |b| {
+        let mut ref_seq = "GATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGA\
+                           TTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTA\
+                           CAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAG\
+                           ATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATT\
+                           ACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACA\
+                           GATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGAT\
+                           TACAGATTACAGATTACAGATTACAGATTACA"
+            .as_bytes()
+            .to_owned();
+
+        struct TestDifferenceModel {}
+        impl SequenceDifferenceModel for TestDifferenceModel {
+            fn new() -> Self {
+                TestDifferenceModel {}
+            }
+            fn get(&self, _i: usize, _read_length: usize, from: u8, to: u8) -> f32 {
+                if from == b'C' && to == b'T' {
+                    return 0.0;
+                } else if from != to {
+                    return -1.0;
+                } else {
+                    return 1.0;
+                }
+            }
+        }
+        let difference_model = TestDifferenceModel::new();
+
+        let parameters = AlignmentParameters {
+            base_error_rate: 0.02,
+            poisson_threshold: 0.04,
+            difference_model,
+            penalty_gap_open: -1.0,
+            penalty_gap_extend: -1.0,
+        };
+
+        // Reference
+        let ref_seq_rev_compl = alphabets::dna::revcomp(ref_seq.iter());
+        ref_seq.extend_from_slice(b"$");
+        ref_seq.extend_from_slice(&ref_seq_rev_compl);
+        drop(ref_seq_rev_compl);
+        ref_seq.extend_from_slice(b"$");
+
+        let alphabet = alphabets::dna::n_alphabet();
+
+        let sa = suffix_array(&ref_seq);
+        let bwtr = bwt(&ref_seq, &sa);
+        let lessa = less(&bwtr, &alphabet);
+        let occ = Occ::new(&bwtr, 3, &alphabet);
+
+        let fm_index = FMIndex::new(&bwtr, &lessa, &occ);
+        let fmd_index = FMDIndex::from(fm_index);
+
+        let mut allowed_mismatches = AllowedMismatches::new(&parameters);
+
+        let patterns = vec![
+            "TAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAG"
+                .as_bytes()
+                .to_owned();
+            100
+        ];
+        let base_qualities = [0, 0, 0, 0];
+
+        for pattern in patterns.iter() {
+            b.iter(|| {
+                k_mismatch_search(
+                    &pattern,
+                    &base_qualities,
+                    allowed_mismatches.get(pattern.len()),
+                    &parameters,
+                    &fmd_index,
+                )
+            })
+        }
+    });
+}
+
+fn bench_exogenous_reads(c: &mut Criterion) {
+    c.bench_function("bench_exogenous_reads", |b| {
+        let mut ref_seq = "GATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGA\
+                           TTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTA\
+                           CAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAG\
+                           ATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATT\
+                           ACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACA\
+                           GATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGAT\
+                           TACAGATTACAGATTACAGATTACAGATTACA"
+            .as_bytes()
+            .to_owned();
+
+        struct TestDifferenceModel {}
+        impl SequenceDifferenceModel for TestDifferenceModel {
+            fn new() -> Self {
+                TestDifferenceModel {}
+            }
+            fn get(&self, _i: usize, _read_length: usize, from: u8, to: u8) -> f32 {
+                if from == b'C' && to == b'T' {
+                    return 0.0;
+                } else if from != to {
+                    return -1.0;
+                } else {
+                    return 1.0;
+                }
+            }
+        }
+        let difference_model = TestDifferenceModel::new();
+
+        let parameters = AlignmentParameters {
+            base_error_rate: 0.02,
+            poisson_threshold: 0.04,
+            difference_model,
+            penalty_gap_open: -1.0,
+            penalty_gap_extend: -1.0,
+        };
+
+        // Reference
+        let ref_seq_rev_compl = alphabets::dna::revcomp(ref_seq.iter());
+        ref_seq.extend_from_slice(b"$");
+        ref_seq.extend_from_slice(&ref_seq_rev_compl);
+        drop(ref_seq_rev_compl);
+        ref_seq.extend_from_slice(b"$");
+
+        let alphabet = alphabets::dna::n_alphabet();
+
+        let sa = suffix_array(&ref_seq);
+        let bwtr = bwt(&ref_seq, &sa);
+        let lessa = less(&bwtr, &alphabet);
+        let occ = Occ::new(&bwtr, 3, &alphabet);
+
+        let fm_index = FMIndex::new(&bwtr, &lessa, &occ);
+        let fmd_index = FMDIndex::from(fm_index);
+
+        let mut allowed_mismatches = AllowedMismatches::new(&parameters);
+
+        let patterns = vec![
+            "TTTTTTTTTTGGGGGTTACAGATTACAGATTACAGGGGGGTTTTTTTTTT"
+                .as_bytes()
+                .to_owned();
+            100
+        ];
+        let base_qualities = [0, 0, 0, 0];
+
+        for pattern in patterns.iter() {
+            b.iter(|| {
+                k_mismatch_search(
+                    &pattern,
+                    &base_qualities,
+                    allowed_mismatches.get(pattern.len()),
+                    &parameters,
+                    &fmd_index,
+                )
+            })
+        }
+    });
+}
+
+criterion_group!(
+    benches,
+    criterion_benchmark,
+    bench_multiple_reads,
+    bench_exogenous_reads
+);
 criterion_main!(benches);
