3e8bacad4bdb37de3e111e2e0be2d306e3ad9f71
J. Christian Heide <christian_heide@eva.mpg.de>
2023-04-19 15:07:25 +0200

    Fix a clippy hint

--- src/map/input_chunk_reader.rs
+++ src/map/input_chunk_reader.rs
@@ -121,9 +121,11 @@ impl InputSource {
         if buf[..2] == GZIP_MAGIC_NUMBER {
             let mut reader = bgzf::Reader::new(src);
             let mut buf = [0; 4];
-            if let Err(_) = reader.read_exact(&mut buf) {
+
+            // Read exactly four bytes to see whether they contain the BAM magic numbers
+            if reader.read_exact(&mut buf).is_err() {
                 // We are going to ignore this error, since we probably got GZ instead of BGZF data.
-                // If that is not the case, potential errors are properly handled elsewhere.
+                // If that is not the case, potential errors are properly handled somewhere else.
             }
 
             return if buf == BAM_MAGIC_NUMBER {
